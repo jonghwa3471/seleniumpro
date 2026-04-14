0faf373f2b4f13fa592287b49289cd55fc6f8092
Jonghwa <whdghk3471@gmail.com>
2026-04-14 12:19:31 +0900

    #1.2 Finding Results

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 
-browser: WebDriver = webdriver.Chrome()
+options = Options()
+options.add_argument("--disable-blink-features=AutomationControlled")
+browser: WebDriver = webdriver.Chrome(options=options)
 
 browser.get("https://google.com")
 
@@ -16,9 +19,13 @@ search_bar.send_keys("hello!")
 search_bar.send_keys(Keys.ENTER)
 
 search_results = WebDriverWait(browser, 10).until(
-    EC.presence_of_element_located((By.CLASS_NAME, "MjjYud"))
+    EC.presence_of_all_elements_located((By.CLASS_NAME, "MjjYud"))
 )
 
-print(search_results)
-
-input("pause")
+for search_result in search_results:
+    try:
+        title = search_result.find_element(By.TAG_NAME, "h3")
+        if title:
+            print(title.text)
+    except:
+        continue
