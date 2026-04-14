69760c3ee37532dab82c0d72df647bea8f24e21b
Jonghwa <whdghk3471@gmail.com>
2026-04-14 14:19:46 +0900

    #1.3 Taking Screenshots

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 
+KEYWORD = "buy domain"
+
 options = Options()
 options.add_argument("--disable-blink-features=AutomationControlled")
 browser: WebDriver = webdriver.Chrome(options=options)
@@ -15,17 +17,15 @@ browser.get("https://google.com")
 search_bar = WebDriverWait(browser, 10).until(
     EC.presence_of_element_located((By.CLASS_NAME, "gLFyf"))
 )
-search_bar.send_keys("hello!")
+search_bar.send_keys(KEYWORD)
 search_bar.send_keys(Keys.ENTER)
 
 search_results = WebDriverWait(browser, 10).until(
     EC.presence_of_all_elements_located((By.CLASS_NAME, "MjjYud"))
 )
 
-for search_result in search_results:
-    try:
-        title = search_result.find_element(By.TAG_NAME, "h3")
-        if title:
-            print(title.text)
-    except:
-        continue
+for index, search_result in enumerate(search_results):
+    if search_result.is_displayed() and search_result.size["height"] > 0:
+        search_result.screenshot(f"screenshots/{KEYWORD}x{index}.png")
+
+browser.quit()
