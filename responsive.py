d9e02334e833f5dfdeef058ae89f21533b803bb1
Jonghwa <whdghk3471@gmail.com>
2026-04-15 11:01:04 +0900

    #2.1 Scrolling Down

--- responsive.py
+++ responsive.py
@@ -1,18 +1,38 @@
 import time
+from math import ceil
 from selenium import webdriver
 from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+
+BROWSER_HEIGHT = 949
 
 browser: WebDriver = webdriver.Chrome()
 
 browser.get("https://nomadcoders.co")
+
 browser.maximize_window()
 
-sizes = [320, 480, 960, 1366, 1920]
+time.sleep(6)
+ActionChains(browser).send_keys(Keys.ESCAPE).perform()
+
+
+sizes = [480, 960, 1366, 1920]
 
 # print(browser.get_window_size())
 
 for size in sizes:
-    browser.set_window_size(size, 949)
-    time.sleep(5)
+    browser.set_window_size(size, BROWSER_HEIGHT)
+    time.sleep(3)
+    scroll_size = browser.execute_script("return document.body.scrollHeight")
+    total_sections = ceil(scroll_size / BROWSER_HEIGHT)
+
+    for section in range(total_sections):
+        browser.execute_script(f"window.scrollTo(0, {(section + 1) * BROWSER_HEIGHT})")
+        time.sleep(2)
+
 
 input("pause")
